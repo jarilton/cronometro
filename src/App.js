2da67e259a60a94136a07892e77da0f26bf78901
jarilton <jariltonpereira@hotmail.com>
2022-01-16 17:42:36 -0300

    implementado a função pausar.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      numero: 0
+      numero: 0,
+      botao: 'Iniciar'
     }
 
     this.timer = null
@@ -15,11 +16,23 @@ class App extends Component {
   }
 
   iniciar() {
-    this.timer = setInterval(() => {
-      let state = this.state
-      state.numero += 0.1
-      this.setState(state)
-    }, 100)
+
+    let state = this.state
+
+    if(this.timer !== null) {
+      clearInterval(this.timer)
+      this.timer = null
+      state.botao = 'Iniciar'
+    } else {
+      this.timer = setInterval(() => {
+        let state = this.state
+        state.numero += 0.1
+        this.setState(state)
+      }, 100)
+      state.botao = 'Pausar'
+    }
+
+    this.setState(state)
   }
 
   limpar() {
@@ -35,7 +48,7 @@ class App extends Component {
       <a className="timer" >{this.state.numero.toFixed(1)}</a>
 
       <div className="areaBtn" >
-        <a className="botao " onClick={this.iniciar} >Iniciar</a> 
+        <a className="botao " onClick={this.iniciar} >{this.state.botao}</a> 
         <a className="botao" onClick={this.limpar} >Limpar</a>
       </div>
 
