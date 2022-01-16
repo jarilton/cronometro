f585cc7bbb4d50641d0b092fa1ee861726b2f5eb
jarilton <jariltonpereira@hotmail.com>
2022-01-16 17:25:34 -0300

    implementado o estado e mostrando resultado no log

--- src/App.js
+++ src/App.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      numero: 0
+    }
+
+    this.iniciar = this.iniciar.bind(this)
+    this.limpar = this.limpar.bind(this)
+  }
+
+  iniciar() {
+    alert('cronômetro iniciado...')
+  }
+
+  limpar() {
+    alert('cronômetro limpado com sucesso!!')
+  }
   
   render() {
     return (
@@ -9,11 +27,11 @@ class App extends Component {
       <h1 className="titulo">Cronometro</h1>
 
       <img src={require("./assets/cronometro.png")} className="img" />
-      <a className="timer" >0.0</a>
+      <a className="timer" >{this.state.numero.toFixed(1)}</a>
 
       <div className="areaBtn" >
-        <a className="botao " >Iniciar</a> 
-        <a className="botao" >Limpar</a>
+        <a className="botao " onClick={this.iniciar} >Iniciar</a> 
+        <a className="botao" onClick={this.limpar} >Limpar</a>
       </div>
 
 
