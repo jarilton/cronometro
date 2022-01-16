cd084c5475140e938db9e9f79cdd1da927598b18
jarilton <jariltonpereira@hotmail.com>
2022-01-16 17:13:33 -0300

    Titulo, cor de fundo e imagem do cronometro.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ class App extends Component {
   
   render() {
     return (
-      <div >
-        
+      <div className="container" >
+      <h1 className="titulo">Cronometro</h1>
+
+      <img src={require("./assets/cronometro.png")} className="img" />
+      <a className="timer" >0.0</a>
+
+      <div className="areaBtn" >
+        <a className="botao " >Iniciar</a> 
+        <a className="botao" >Limpar</a>
+      </div>
+
+
       </div>
     );
   }
