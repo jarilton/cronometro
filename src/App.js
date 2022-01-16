40912ce5e5b030e375642b229210596b6344a1f7
jarilton <jariltonpereira@hotmail.com>
2022-01-16 17:31:24 -0300

    cronometro iniciado.

--- src/App.js
+++ src/App.js
@@ -9,12 +9,17 @@ class App extends Component {
       numero: 0
     }
 
+    this.timer = null
     this.iniciar = this.iniciar.bind(this)
     this.limpar = this.limpar.bind(this)
   }
 
   iniciar() {
-    alert('cronômetro iniciado...')
+    this.timer = setInterval(() => {
+      let state = this.state
+      state.numero += 0.1
+      this.setState(state)
+    }, 100)
   }
 
   limpar() {
